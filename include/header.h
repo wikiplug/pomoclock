a361aa32125fa2afb71c79b9277cc73d32084099
wiki-joaquin <joaquin.s.cardenas4@gmail.com>
2022-07-16 15:55:14 -0300

    actualizando platform.ini, main y .gitgnore. agregando header.h

---
+++ include/header.h
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>		// incluye libreria para interfaz I2C
+#include <RTClib.h>		// incluye libreria para el manejo del modulo RTC
+#include <TwiLiquidCrystal.h>
