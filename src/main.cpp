525c73e32255108be893ec47a7e38acd351832d3
wiki-joaquin <joaquin.s.cardenas4@gmail.com>
2022-07-16 16:29:37 -0300

    actualizando main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,67 @@
-#include <Arduino.h>
+#include "header.h"
 
-void setup() {
-  // put your setup code here, to run once:
+bool button_2;
+bool pomoSwitch;
+bool pomoSwitchRead;
+bool startPomo;
+bool selectButton;
+bool selectButtonRead;
+byte menuPosition;
+
+void setup(){
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+    pomodoro();
+}
+
+// EN ESTA FUNCIÓN SE CORROBORA SI SE ACTIVA LA OPCION POMODORO O NO
+// A SU VEZ, EJECUTA TODAS LAS FUNCIONES QUE CORREN DENTRO DEL POMODORO
+// SE SEGMENTA EL CODIGO EN BLOQUES PARA FACILITAR SU LEGIBILIDAD Y PROGRAMACION
+void pomodoro(){ 
+    pomoSwitchRead = digitalRead(pomoSwitch);
+    
+    if(pomoSwitchRead == HIGH){
+        if(startPomo == LOW){ //SI LA OPCION DE START NO FUE SELECCIONADA ANTERIORMENTE SE MUESTRA EL MENU POMODORO
+            pomo_menu();
+        }
+        else{
+            work_or_break(); //FUNCION QUE DICTAMINA SI ES TIEMPO DE TRABAJO O DESCANSO 
+        }
+    }
+
+    else{ //REGRESO TODAS LAS VARIABLES NECESARIAS PARA EL CORRECTO INICIO DEL POMODORO A SU VALOR DEFAULT.
+        startPomo = LOW;
+    }
+}
+
+void pomo_menu(){
+    selectButtonRead = digitalRead(button_2);
+    if(selectButtonRead == HIGH){
+        delay(15);
+        menuPosition++;
+    } 
+    if(menuPosition > 3){
+        menuPosition = 0;
+    }
+    
+    select_work();    
+    select_break();
+    select_sessions();
+    select_start();
+
+}
+
+void select_work(){
+    if(menuPosition == 0){
+        //parpadee e incremente
+        if(menuPosition)
+
+    }
+    lcd.setCursor(0, 0);
+    lcd.print("WORK");
+
+    lcd.setCursor(0, 1);
+    lcd.print(WORK_TIME);
 }
